5c14fb840c28584c1ee8eaa22b2ed5f53d681531
Justin Horner <jhorner@pepid.com>
2014-04-18 12:08:33 -0700

    main is pulling max date correctly

--- main.py
+++ main.py
@@ -9,30 +9,35 @@ import ConfigParser
 - Method to insert scrobbles into db
 """
 
-last_fm_api_key = ''
-last_fm_user = ''
-sql_pass = ''
-sql_user = ''
+class vars:
+    last_fm_api_key = ''
+    last_fm_user = ''
+    sql_pass = ''
+    sql_user = ''
+    sql_srv = ''
+    sql_db = ''
+
 
 def xecute():
     load_config()
     _date = getMaxDate()
-    getScrobbles("")
+    getScrobbles(_date)
 
 """
     Gets the
 """
-def getScrobbles(page):
-    api_url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&format=json&page=%s' % (last_fm_user, last_fm_api_key, page)
+def getScrobbles(_date):
+    page = '1'
+    api_url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=%s&api_key=%s&format=json&page=%s' % (vars.last_fm_user, vars.last_fm_api_key, vars.page)
 
 
 """Gets the last max scrobble date from db"""
 def getMaxDate():
     print 
-    cnxn = pyodbc.connect('DRIVER={PostgreSQL Unicode};SERVER=justinhorner.me;DATABASE=mypgdb;UID=' + sql_user + ';PWD=' + sql_pass)
+    cnxn = pyodbc.connect('DRIVER={PostgreSQL Unicode};SERVER=%s;DATABASE=%s;UID=%s;PWD=%s;SSLMODE=require' % (vars.sql_srv, vars.sql_db, vars.sql_user, vars.sql_pass))
     cursor = cnxn.cursor()
     rows = cursor.execute('select max(unixtime) from public.lastfm_scrobbles;').fetchone()
-    date = rows
+    date = row
     print rows
 
     return date
@@ -42,10 +47,13 @@ def load_config():
     configFilePath = r'config'
     configParser.read(configFilePath)
 
-    last_fm_api_key = configParser.get('configs', 'last_fm_api_key')
-    last_fm_user = configParser.get('configs', 'last_fm_user')
-    sql_user = configParser.get('configs', 'sql_user')
-    sql_pass = configParser.get('configs', 'sql_pass')
+    vars.last_fm_api_key = configParser.get('configs', 'last_fm_api_key')
+    vars.last_fm_user = configParser.get('configs', 'last_fm_user')
+    vars.sql_user = configParser.get('configs', 'sql_user')
+    vars.sql_pass = configParser.get('configs', 'sql_pass')
+    vars.sql_srv = configParser.get('configs', 'sql_srv')
+    vars.sql_db = configParser.get('configs', 'sql_db')
+
 
 
 
